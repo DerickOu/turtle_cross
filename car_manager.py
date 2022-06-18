b93e061babbf92710e74ef2d2aa418fb9c3345b1
schoolimportant001 <107427987+schoolimportant001@users.noreply.github.com>
2022-06-18 18:15:05 -0400

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 import random
 
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+https://github.com/schoolimportant001/turtle_cross/blob/main/car_manager.pyCOLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
